426eb111bfd71b87f7bee77c92d2e0d0fd6a7ed4
Paula Cotroneo <paula.y.cotroneo@gmail.com>
2022-02-18 17:56:59 -0300

    Desafío clase 13: Item Collection II

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,62 @@
 import { useCartContext } from '../../context/CartContext';
 import { Link } from 'react-router-dom';
+import { getFirestore, collection, addDoc, query, where, documentId, writeBatch, getDocs } from 'firebase/firestore'
 import '../../App.css'
 
 
 export const Cart = () => {
 
-  const { cartList, precioTotal, eliminarDelCarrito, vaciarCarrito } = useCartContext()
+  const { cartList, precioTotal, eliminarDelCarrito, vaciarCarrito } = useCartContext();
+
+  const realizarCompra = async (e) => {
+    e.preventDefault()  
+         
+    let orden = {}
+       
+    orden.buyer = {name:'Paula',email: 'pau@gmail.com', phone: '1234567890'}
+    orden.items = cartList.map(cartItem => {
+        const id = cartItem.item.id;
+        const nombre = cartItem.item.nombre;
+        const precio = cartItem.item.precio * cartItem.cantidad;
+        const cantidad = cartItem.cantidad
+        
+        return {
+            id, 
+            nombre, 
+            precio, 
+            cantidad
+        }   
+    }) 
+    orden.total = precioTotal(); 
+
+    console.log(orden)
+    
+    const db = getFirestore()
+    const ordersCollection = collection (db, 'orders')
+    await addDoc( ordersCollection, orden)
+    .then(resp => console.log(resp))
+    
+    const queryCollection = collection(db, 'items')
+        
+    const queryActulizarStock = query(
+        queryCollection, 
+        where ( documentId() , 'in', cartList.map(it => it.item.id) )          
+    ) 
+
+    const batch = writeBatch(db)
+
+    await getDocs(queryActulizarStock)
+    .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
+            stock: res.data().stock - cartList.find(item => item.item.id === res.id).cantidad
+         })
+    ))
+    .catch(err => console.log(err))
+    .finally(() =>console.log('Se actualiza el Stock'))
+
+    batch.commit()
+
+  }
+
 
   return (
     <div className="contenedor">
@@ -27,6 +78,7 @@ export const Cart = () => {
           {`Total a Pagar: $${precioTotal()}`}
         </div>
         <button className="carrito__btn" onClick={vaciarCarrito}>Vaciar Carrito</button>
+        <button className="carrito__btn" onClick={realizarCompra}>Realizar Orden</button>
         </>
       :   
         <>   
